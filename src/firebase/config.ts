26974ad7d31fad270fbe5cde848b0f8de03e8734
Adebayo Precious <padebayo236@gmail.com>
2026-02-26 00:52:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6240043784-f7c17",
+  "appId": "1:903215530778:web:5f9a6fd06fd4f00cf562a3",
+  "apiKey": "AIzaSyB3Brtiqc869VnYRbv3ujfonMpKahzG1-0",
+  "authDomain": "studio-6240043784-f7c17.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "903215530778"
+};
